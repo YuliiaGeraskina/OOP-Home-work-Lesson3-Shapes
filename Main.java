f097f4e2a4c0937cf78d15dafa2fd8e1134be45e
YuliiaGeraskina <julia.ulik@gmail.com>
2015-03-16 13:33:39 +0200

    Update Main.java
    Построить иерархию классов «Фигуры» с учетом знаний о
    полиморфизме. У каждого класса фигуры должен быть
    метод подсчета площади. Создать список фигур. Вывести
    площади всех фигур на экран.

--- Main.java
+++ Main.java
@@ -1,12 +1,46 @@
-*.class
+public class Main {
+	public static void main(String[] args) {
+		Shape[] list = {
+				new Rectangle(1+(int)(Math.random() * 50), 1+(int)(Math.random() * 50)),
+				new Rectangle(1+(int)(Math.random() * 50), 1+(int)(Math.random() * 50)),
+				new Triangle(1+(int)(Math.random() * 50), 1+(int)(Math.random() * 50), 1+(int)(Math.random() * 50)),
+				new Circle(1+(int)(Math.random() * 50))		
+		};
+		
 
-# Mobile Tools for Java (J2ME)
-.mtj.tmp/
+		StringBuilder sb;
+		double Areas = 0;
+		double AreaRect = 0;
+		double AreaTriangle = 0;
+		double AreaCircle = 0;
+		for (Shape s : list) {
+			sb = new StringBuilder();
+			sb.append(s.getShape());
+			sb.append("[" + s.getShapeParametrs() + "]");
+			sb.append(": ");
+			sb.append(s.getArea());
+		
+			System.out.println(sb.toString());
+			
+			if(s.getShape() == "Rectangle"){
+				AreaRect = s.getArea() + AreaRect;
+			}
+			if(s.getShape() == "Triangle"){
+				AreaTriangle = s.getArea() + AreaTriangle;
+			}
+			if(s.getShape() == "Circle"){
+				AreaCircle = s.getArea() + AreaCircle;
+			}
+			s.Draw();
 
-# Package Files #
-*.jar
-*.war
-*.ear
+		}
+		System.out.println("Summary area of all Rectangles: " + AreaRect);
+		System.out.println("Summary area of all Triangles: " + AreaTriangle);
+		System.out.println("Summary area of all Circles: " + AreaCircle);
+		Areas = AreaRect + AreaTriangle + AreaCircle;
+		System.out.println("Summary area of all shapes: " + Areas);
+	}
+		
 
-# virtual machine crash logs, see http://www.java.com/en/download/help/error_hotspot.xml
-hs_err_pid*
+	
+}
